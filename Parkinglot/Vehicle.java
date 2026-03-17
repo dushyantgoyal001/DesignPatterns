621a0d085d326547914051b3428ecd6513e44951
dushyant goyal <dushyant@adobe.com>
2026-03-17 20:27:31 +0530

    add car class

--- Parkinglot/Vehicle.java
+++ Parkinglot/Vehicle.java
@@ -3,4 +3,9 @@ package Parkinglot;
 public abstract class  Vehicle {
     private String licensePlate;
     private String vehicleType;
+
+    public Vehicle(String licensePlate, VehicleType vehicleType) {
+        this.licensePlate = licensePlate;
+        this.vehicleType = vehicleType.name();
+    }
 }
